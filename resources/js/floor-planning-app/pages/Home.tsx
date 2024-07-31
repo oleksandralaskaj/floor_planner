b346e85f6b97a489a244837d4ba96a4a9a891d43
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-31 14:27:13 +0200

    change styling of picture on home page, add non-breakable spaces in text

--- resources/js/floor-planning-app/pages/Home.tsx
+++ resources/js/floor-planning-app/pages/Home.tsx
@@ -11,20 +11,15 @@ export const Home = () => {
             <div className={styles.textBlock}>
                 <h1 className={styles.title}>Floor planner</h1>
                 <h2 className={styles.subtitle}>An app created as a web development bootcamp final project, striving to
-                    solve a real issue. </h2>
+                    solve a&nbsp;real issue. </h2>
                 <p className={styles.paragraph}>Floor Planner emerged from my desire to bridge the gap for
-                    non-professionals
-                    seeking an intuitive floor plan tool. As a junior developer, I empathized with real estate agents
-                    and
-                    homeowners struggling to visualize spaces. Inspired by their challenges, I embarked on a mission to
+                    non-professionals seeking an&nbsp;intuitive floor plan tool. As&nbsp;a&nbsp;junior developer, I empathized with real estate agents&nbsp;and homeowners struggling to&nbsp;visualize spaces. Inspired by their challenges, I embarked on&nbsp;a&nbsp;mission to
                     create a user-friendly solution.
                 </p>
                 <p className={styles.paragraph}>
                     Leveraging React, Typescript, and Laravel, Floor Planner was born—a
-                    platform that simplifies floor plan creation. With its drag-and-drop interface, users can sketch
-                    rooms,
-                    add doors, windows, and even resize elements. Whether you’re planning your dream home or preparing a
-                    property listing, Floor Planner empowers everyone to design with confidence.
+                    platform that&nbsp;simplifies floor plan creation. With its drag-and-drop interface, users can sketch
+                    rooms, add doors, windows, and even resize elements. Whether you’re planning your dream home or preparing a&nbsp;property listing, Floor Planner empowers everyone to design with&nbsp;confidence.
                 </p>
                 <Link to={'/planner'}><Button type={'active'}>Start drawing</Button></Link>
             </div>
